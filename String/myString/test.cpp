8369503eb02d72287e79e7a5b8b25e718147f906
LINGLUO00 <3425375449@qq.com>
2024-12-19 19:26:47 +0800

    change file name

---
+++ String/myString/test.cpp
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+#include<vector>
+#include<list>
+#include<string>
+using namespace std;
+
+#include"string.h"
+
+int main()
+{
+
+	return 0;
+}
